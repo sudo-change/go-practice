c97a251d3d6d897ca1148b6ecdf91cc9046ee49d
sudo change <63948824+sudo-change@users.noreply.github.com>
2022-02-27 23:03:50 +0530

    Create switchCase2.go

---
+++ switchCase2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func time() int {
+	return 6
+}
+
+func main() {
+
+	switch work := time(); { // if the variable declared here, it will be local to this only
+	case work < 6:
+		fmt.Println("more focus")
+	case work < 10:
+		fmt.Println("work in focused burst to use time effectively")
+	default:
+		fmt.Println("other things dont worth your time")
+	}
+
+	fmt.Println(work) //can't access work
+
+}
