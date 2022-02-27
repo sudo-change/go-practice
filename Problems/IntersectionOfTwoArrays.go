8da34dadc3cb9134320420639217e411d65a4eca
sudo change <63948824+sudo-change@users.noreply.github.com>
2022-02-27 23:26:14 +0530

    Update IntersectionOfTwoArrays.go

--- Problems/IntersectionOfTwoArrays.go
+++ Problems/IntersectionOfTwoArrays.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func Intersection(arr1, arr2 []int) []int {
 	test := make(map[int]bool)
-
+	// making a hash table
 	for _, value := range arr1 {
 		test[value] = true
 	}
 
 	var result []int
 	for _, value := range arr2 {
+		// checking for values in arr2 exists in arr1 hashtaable or not
+		// looking for a vlue in hash table takes constant amount of time
 		if _, ok := test[value]; ok {
+			// adding matched values to a slice
 			result = append(result, value)
 		}
 	}
